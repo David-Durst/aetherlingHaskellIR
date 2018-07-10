625f221f22a5f6981bfe1be90ed6e8d5ea63136f
akeley98 <dza724@gmail.com>
2018-07-09 17:23:25 -0700

    Fix LineBuffer outPorts. Replace SequenceArrayController with substitute types in examples.

--- src/examples/Examples.hs
+++ src/examples/Examples.hs
@@ -33,11 +33,13 @@ memReadInt = MemRead T_Int
 
 memWriteInt = MemWrite T_Int
 
-sac2Int = SequenceArrayController [(1, (T_Array 2 T_Int))] [(2, T_Int)]
+spaceAndTimeReshape = SequenceArrayRepack (1, 2, T_Int) (2, 1, T_Int) |>>=|
+  ArrayReshape [T_Array 1 T_Int] [T_Int]
 
-constantToSAC = 
+constantSpaceTimeReshape = 
   Underutil 3 (Constant_Int [1, 1, 1]) |>>=| 
-  SequenceArrayController [(1, (T_Array 3 T_Int))] [(3, T_Int)]
+  SequenceArrayRepack (1, 3, T_Int) (3, 1, T_Int) |>>=|
+  ArrayReshape [T_Array 1 T_Int] [T_Int]
 
 duplicateAdd = DuplicateOutputs 3 (Add T_Int)
 
@@ -45,9 +47,9 @@ conv1PxPerClock =
   (
     (
       MemRead T_Int |>>=|
-      SequenceArrayController [(1, T_Int)] [(1, T_Array 1 T_Int)] |>>=|
+      ArrayReshape [T_Int] [T_Array 1 T_Int] |>>=|
       LineBuffer [1] [3] [300] T_Int |>>=|
-      SequenceArrayController [(1, T_Array 1 (T_Array 3 T_Int))] [(1, T_Array 3 T_Int)]
+      ArrayReshape [T_Array 1 (T_Array 3 T_Int)] [T_Array 3 T_Int]
     ) |&|
     Constant_Int [1, 1, 1]
   ) |>>=|
