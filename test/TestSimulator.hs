4d37b94333efae48b16ab6cf9cb02db5ed783ff0
akeley98 <dza724@gmail.com>
2018-07-26 13:47:37 -0700

    Remove unneeded monad syntax in TestSimulator.hs

--- test/TestSimulator.hs
+++ test/TestSimulator.hs
@@ -46,18 +46,20 @@ data SimhlRand = SimhlRand { simhlRandState :: Int }
 
 simhlRandInt :: SimhlRand -> (SimhlRand, Int)
 simhlRandInt (SimhlRand state) =
-  do { let x1 = (shiftL state 13) `xor` state
-     ; let x2 = (shiftR x1 17) `xor` x1
-     ; let x3 = (shiftL x2 5)  `xor` x2
-     ; let x32bits = x3 .&. 0xFFFFFFFF
-     ; (SimhlRand x32bits, x32bits .&. 0x000000FF)
-  }
+  let
+    x1 = (shiftL state 13) `xor` state
+    x2 = (shiftR x1 17) `xor` x1
+    x3 = (shiftL x2 5)  `xor` x2
+    x32bits = x3 .&. 0xFFFFFFFF
+  in
+    (SimhlRand x32bits, x32bits .&. 0x000000FF)
 
 simhlRandBool :: SimhlRand -> (SimhlRand, Bool)
 simhlRandBool inRand =
-  do { let (outRand, x) = simhlRandInt inRand
-     ; (outRand, x .&. 1 == 1)
-  }
+  let
+    (outRand, x) = simhlRandInt inRand
+  in
+    (outRand, x .&. 1 == 1)
 
 -- Get a sequence of seqLen random ValueTypes, suitable for simulating
 -- an op with an input port with given TokenType. Returns the random
@@ -66,31 +68,36 @@ simhlRandValues :: SimhlRand -> Int -> TokenType -> (SimhlRand, [ValueType])
 simhlRandValues inRand 0 _ = (inRand, [])
 simhlRandValues inRand seqLen T_Unit = (inRand, replicate seqLen V_Unit)
 simhlRandValues inRand seqLen T_Int =
-  do { let (nextRand, theInt) = simhlRandInt inRand
-     ; let (outRand, moreValues) = simhlRandValues nextRand (seqLen-1) T_Int
-     ; (outRand, (V_Int theInt):moreValues)
-  }
+  let
+    (nextRand, theInt) = simhlRandInt inRand
+    (outRand, moreValues) = simhlRandValues nextRand (seqLen-1) T_Int
+  in
+    (outRand, (V_Int theInt):moreValues)
+
 simhlRandValues inRand seqLen T_Bit =
-  do { let (nextRand, theBool) = simhlRandBool inRand
-     ; let (outRand, moreValues) = simhlRandValues nextRand (seqLen-1) T_Bit
-     ; (outRand, (V_Bit theBool):moreValues)
-  }
+  let
+    (nextRand, theBool) = simhlRandBool inRand
+    (outRand, moreValues) = simhlRandValues nextRand (seqLen-1) T_Bit
+  in
+    (outRand, (V_Bit theBool):moreValues)
+
 simhlRandValues inRand seqLen (T_Array n t) =
-  do { let (nextRand, array) = simhlRandValues inRand n t
-     ; let (outRand, moreArrays) = simhlRandValues nextRand (seqLen-1) (T_Array n t)
-     ; (outRand, (V_Array array):moreArrays)
-  }
+  let
+    (nextRand, array) = simhlRandValues inRand n t
+    (outRand, moreArrays) = simhlRandValues nextRand (seqLen-1) (T_Array n t)
+  in
+    (outRand, (V_Array array):moreArrays)
 
 -- Look at the op's input ports and generate suitable random input.
 simhlOpRandInputs :: SimhlRand -> Op -> Int -> (SimhlRand, [[ValueType]])
 simhlOpRandInputs inRand op seqLen =
-  do { let inTypes = map pTType (inPorts op)
-     ; let foldLambda = \(prevRand, results) t ->
-             do { let (nextRand, values) = simhlRandValues prevRand seqLen t
-                ; (nextRand, results ++ [values])
-             }
-     ; foldl foldLambda (inRand, []) inTypes
-  }
+  let
+    inTypes = map pTType (inPorts op)
+    foldLambda = \(prevRand, results) t ->
+      let (nextRand, values) = simhlRandValues prevRand seqLen t
+      in (nextRand, results ++ [values])
+  in
+    foldl foldLambda (inRand, []) inTypes
 
 -- Similar for a list of (sequence length :: Int, type :: TokenType)
 -- Used for generating memory inputs, see SimhlTestCase.
@@ -98,11 +105,11 @@ simhlMemRandInputs :: SimhlRand -> [(Int, TokenType)]
                    -> (SimhlRand, [[ValueType]])
 simhlMemRandInputs inRand [] = (inRand, [])
 simhlMemRandInputs inRand (tuple:tuples) =
-  do { let (nextRand, value) = simhlRandValues inRand (fst tuple) (snd tuple)
-     ; let (outRand, values) = simhlMemRandInputs nextRand tuples
-     ; (outRand, value:values)
-  }
-  
+  let
+    (nextRand, value) = simhlRandValues inRand (fst tuple) (snd tuple)
+    (outRand, values) = simhlMemRandInputs nextRand tuples
+  in
+    (outRand, value:values)
 
 -- Test case data for the simulator tests.  Includes a description for
 -- the op being tested, the op, a manually-implemented function that
@@ -121,17 +128,16 @@ data SimhlTestCase = SimhlTestCase {
 simhlMakeTestCases :: SimhlRand -> [SimhlTestCase] -> [TestTree]
 simhlMakeTestCases _ [] = []
 simhlMakeTestCases inRand (thisCase:cases) =
-  do { let op = simhlTestOp thisCase
-     ; let (memRand, inSeqs) = simhlOpRandInputs inRand op
-                                                 (simhlInStrLen thisCase)
-     ; let (nextRand, inMem) = simhlMemRandInputs memRand
-                                                  (simhlMemTypes thisCase)
-     ; let tree = testCase (simhlTestDescription thisCase) $
-                  (simulateHighLevel op inSeqs inMem)
-              @?= ((simhlTestImpl thisCase) inSeqs inMem)
-     ; let trees = simhlMakeTestCases nextRand cases
-     ; tree:trees
-  }
+  let
+    op = simhlTestOp thisCase
+    (memRand, inSeqs) = simhlOpRandInputs inRand op (simhlInStrLen thisCase)
+    (nextRand, inMem) = simhlMemRandInputs memRand (simhlMemTypes thisCase)
+    tree = testCase (simhlTestDescription thisCase) $
+           (simulateHighLevel op inSeqs inMem)
+           @?= ((simhlTestImpl thisCase) inSeqs inMem)
+    trees = simhlMakeTestCases nextRand cases
+  in
+    tree:trees
 
 -- Take two vec4s, subtract them, and double the result.
 -- Note that the implementation function has a bunch of ugly list
@@ -142,14 +148,15 @@ vec4SubTimes8Op = MapOp 4 ((Sub T_Int) |>>=| (Shl 3 T_Int))
 vec4SubTimes8Impl :: [[ValueType]] -> [[ValueType]]
                   -> ( [[ValueType]], [[ValueType]] )
 vec4SubTimes8Impl inSeqs _ =
-  do { let [leftSeq, rightSeq] = inSeqs
-     ; ([[V_Array
+  let [leftSeq, rightSeq] = inSeqs
+  in
+       ([[V_Array
           [V_Int (8*(a-b)) | (V_Int a, V_Int b) <- zip leftArray rightArray]
         | (V_Array leftArray, V_Array rightArray) <- zip leftSeq rightSeq
         ]],
         []
        )
-  }
+
 simhlCase0 = SimhlTestCase
   "Subtract 4-vec, then multiply by 8. (Tests Sub, Shl, MapOp)"
   vec4SubTimes8Op
@@ -163,23 +170,30 @@ simhlCase0 = SimhlTestCase
 -- the last test case should have ensured simhlCombinational works
 -- correctly.
 simhlCombinationalIgnoreMem :: ([ValueType]->[ValueType])
-                    -> [[ValueType]] -> [[ValueType]]
-                    -> ( [[ValueType]], [[ValueType]] )
-simhlCombinationalIgnoreMem impl inSeqs _ =
-  if any null inSeqs
-  then ([], [])
-  else do { let inputsNow = map head inSeqs
-          ; let inputsL8r = map tail inSeqs
-          ; let outputsNow = impl inputsNow
-          ; let outputsL8r = fst $ simhlCombinationalIgnoreMem impl inputsL8r []
-          ; let allOutputs =
-                 if null outputsL8r
-                 then [[outputNow] | outputNow <- outputsNow] -- 1-seq output case
-                 else                                         -- N-seq output case
-                 [outputNow:outputL8r
-                 |(outputNow, outputL8r) <- zip outputsNow outputsL8r]
-          ; (allOutputs, [])
-       }
+                            -> [[ValueType]] -> [[ValueType]]
+                            -> ( [[ValueType]], [[ValueType]] )
+simhlCombinationalIgnoreMem impl inStrs _ =
+  (simhlCombinational2 impl inStrs, [])
+
+simhlCombinational2 :: ([ValueType]->[ValueType]) -> [[ValueType]] -> [[ValueType]]
+simhlCombinational2 impl inStrs | any null inStrs =
+  error "Aetherling internal error: cannot simulate 0-input-stream device."
+simhlCombinational2 impl inStrs | any (\x -> length x == 1) inStrs =
+  let
+    inputsNow = map head inStrs
+    outputsNow = impl inputsNow
+  in
+    [[outputNow] | outputNow <- outputsNow]
+simhlCombinational2 impl inStrs =
+  let
+    inputsNow = map head inStrs
+    inputsLater = map tail inStrs
+    outputsNow = impl inputsNow
+    outputsLater = simhlCombinational2 impl inputsLater
+  in
+    [outputNow:outputLater
+    |(outputNow, outputLater) <- zip outputsNow outputsLater]
+
 
 -- ((a xor b) / (c|100)) >> 5
 simhlCase1Op =
