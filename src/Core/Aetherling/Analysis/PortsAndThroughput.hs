aab5500651fdf5d923e21e2781940ac03f1239bf
David Durst <davidbdurst@gmail.com>
2018-07-27 11:19:38 -0700

    removed lbwarmup, made linebuffer same input and ouput seqlen, reoganized portsandthrouhgputs

--- src/Core/Aetherling/Analysis/PortsAndThroughput.hs
+++ src/Core/Aetherling/Analysis/PortsAndThroughput.hs
@@ -1,7 +1,7 @@
 {-|
-Module: Ports
+Module: PortsAndThroughput
 Description: Determines the input and output ports of an op, the clocks per 
-sequence used to process the inputs on those ports, and the resulting throughput.
+sequence used to process the inputs on those ports, and the resulting throughput
 -}
 module Aetherling.Analysis.PortsAndThroughput where
 import Aetherling.Operations.Types
@@ -11,40 +11,7 @@ import Aetherling.Analysis.Metrics
 import Data.Bool
 import Data.Ratio
 
-unionPorts :: (Op -> [PortType]) -> [Op] -> [PortType]
-unionPorts portsGetter ops = foldl (++) [] $ map portsGetter ops
-
--- Helper for in and out ports of compose to scale the sequence lengths
--- of all the ports by the same amount as their CPSs are being scaled by
---
--- for each op in the list ops, get all the in or out ports 
--- Then, create a scaling factor for each op, returning flat list with
--- a copy of the scaling factor for an op replicated for each of the ops ports
-getSeqLenScalingsForAllPorts :: Op -> [Op] -> (Op -> [PortType]) -> [Int]
-getSeqLenScalingsForAllPorts containerOp ops portGetter = ssScalings
-  where
-    -- given one op, get the scaling factor for its ports
-    -- will always get int here with right rounding as CPS of overall composePar
-    -- is multiple of cps of each op
-    opScaling op = cps containerOp `ceilDiv` cps op
-    -- given one op, get a the scaling factor repeated for each of its ports
-    -- note: all will be same, just need duplicates
-    opScalingForAllPorts op = replicate (length $ portGetter op) (opScaling op)
-    -- scaling factors for all ports of all ops
-    ssScalings = foldl (++) [] $ map opScalingForAllPorts ops
-
--- update the sequence lengths of a list of ports
-scalePortsSeqLens :: [Int] -> [PortType] -> [PortType]
-scalePortsSeqLens sLenScalings ports = map updatePort $ zip ports sLenScalings
-  where
-    updatePort (T_Port name origSLen tType pct, sLenScaling) = 
-      T_Port name (origSLen * sLenScaling) tType pct
-
-oneInSimplePort t = [T_Port "I" 1 t 1]
-twoInSimplePorts t = [T_Port "I0" 1 t 1,  T_Port "I1" 1 t 1]
-
--- inPorts and outPorts handle the sequence lengths because each port can 
--- have its own
+-- | Compute the in ports of a module.
 inPorts :: Op -> [PortType]
 inPorts (Add t) = twoInSimplePorts t
 inPorts (Sub t) = twoInSimplePorts t
@@ -107,22 +74,7 @@ inPorts cSeq@(ComposeSeq (hd:_)) = renamePorts "I" $
   scalePortsSeqLens (getSeqLenScalingsForAllPorts cSeq [hd] inPorts) (inPorts hd)
 inPorts (Failure _) = []
 
-
-lbWarmup :: [Int] -> [Int] -> [Int] -> Int
--- only guaranteed for 1d and 2d cases
-lbWarmup (pHd:[]) (wHd:[]) _ = (wHd `ceilDiv` pHd) - 1
--- for the 2d case, warmup is filling the entire 1d linebuffer for all but last
--- dimension of window, then doing warmup of 1d linebuffer
-lbWarmup (pHd:pTl) (wHd:wTl) (imgHd:imgTl) =
-  (((wHd `ceilDiv` pHd) - 1) * lowerDimLBInSeqLen) + lowerDimLBWarmup
-  where
-    -- cps is the same as input seq length
-    lowerDimLBInSeqLen = pSeqLen $ head $ inPorts $
-      LineBuffer pTl wTl imgTl T_Int Crop
-    lowerDimLBWarmup = lbWarmup pTl wTl imgTl
-lbWarmup _ _ _ = 0
-
-oneOutSimplePort t = [T_Port "O" 1 t 1]
+-- | Compute the out ports of a module.
 outPorts :: Op -> [PortType]
 outPorts (Add t) = oneOutSimplePort t
 outPorts (Sub t) = oneOutSimplePort t
@@ -158,7 +110,7 @@ outPorts lb@(LineBuffer p w img t _) = [T_Port "O" seqLen parallelStencilType 1]
     parallelStencilType =
       foldr (\pDim innerType -> T_Array pDim innerType) singleStencilType p
     -- seqLen is same as inputs, except with nothing on warmup inputs
-    seqLen = (pSeqLen $ head $ inPorts lb) - lbWarmup p w img
+    seqLen = (pSeqLen $ head $ inPorts lb)
 outPorts (Constant_Int ints) = [T_Port "O" 1 (T_Array (length ints) T_Int) 1]
 outPorts (Constant_Bit bits) = [T_Port "O" 1 (T_Array (length bits) T_Bit) 1]
 
@@ -188,12 +140,55 @@ outPorts cSeq@(ComposeSeq ops) = renamePorts "O" $ scalePortsSeqLens
   where lastOp = last ops
 outPorts (Failure _) = []
 
+-- | commonly used in port that takes a type t in every with 1 seq len so can be
+-- scaled to anything
+oneInSimplePort t = [T_Port "I" 1 t 1]
+-- | commonly used pair of in ports that each take a type t in every with 1 seq
+-- len so can be scaled to anything
+twoInSimplePorts t = [T_Port "I0" 1 t 1,  T_Port "I1" 1 t 1]
+
+-- | commonly used out port that emits t once, can have seq len scaled any
+-- amount as length is just 1
+oneOutSimplePort t = [T_Port "O" 1 t 1]
+
+-- |Helper function that gets all the in or out ports for a collection ops
+-- This is used by composePar to get all the in or out ports from its children 
+unionPorts :: (Op -> [PortType]) -> [Op] -> [PortType]
+unionPorts portsGetter ops = foldl (++) [] $ map portsGetter ops
+
+-- | Helper for in and out ports of compose to scale the sequence lengths
+-- of all the ports by the same amount as their CPSs are being scaled by
+--
+-- for each op in the list ops, get all the in or out ports 
+-- Then, create a scaling factor for each op, returning flat list with
+-- a copy of the scaling factor for an op replicated for each of the ops ports
+getSeqLenScalingsForAllPorts :: Op -> [Op] -> (Op -> [PortType]) -> [Int]
+getSeqLenScalingsForAllPorts containerOp ops portGetter = ssScalings
+  where
+    -- given one op, get the scaling factor for its ports
+    -- will always get int here with right rounding as CPS of overall composePar
+    -- is multiple of cps of each op
+    opScaling op = cps containerOp `ceilDiv` cps op
+    -- given one op, get a the scaling factor repeated for each of its ports
+    -- note: all will be same, just need duplicates
+    opScalingForAllPorts op = replicate (length $ portGetter op) (opScaling op)
+    -- scaling factors for all ports of all ops
+    ssScalings = foldl (++) [] $ map opScalingForAllPorts ops
+
+-- | Scale the sequence lengths of a list of ports by a provided list of
+-- scalings. This is used when scaling ports to match how cps' are scaled. 
+scalePortsSeqLens :: [Int] -> [PortType] -> [PortType]
+scalePortsSeqLens sLenScalings ports = map updatePort $ zip ports sLenScalings
+  where
+    updatePort (T_Port name origSLen tType pct, sLenScaling) = 
+      T_Port name (origSLen * sLenScaling) tType pct
+
 -- | This is shorthand for clocksPerSequence
 cps op = clocksPerSequence op
 -- | A helper constant that defines the CPS for a register
 registerCPS = 1 
 
--- | The function that computes the clocks an op needs to accept the input
+-- | Compute the clocks an op needs to accept the input
 -- sequence to each of its ports assuming the op is fully pipelined.
 -- This is the time in a throughput computation.
 clocksPerSequence :: Op -> Int
@@ -218,7 +213,7 @@ clocksPerSequence Gt = combinationalCPS
 clocksPerSequence Geq = combinationalCPS
 clocksPerSequence (LUT _) = combinationalCPS
 
--- to what degree can we pipeline MemRead and MemWrite
+-- NOTE: to what degree can we pipeline MemRead and MemWrite?
 clocksPerSequence (MemRead _) = combinationalCPS 
 clocksPerSequence (MemWrite _) = combinationalCPS 
 
