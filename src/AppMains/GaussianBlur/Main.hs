302a93dc01ab572fb3f83eb09bc462c7619909bc
akeley98 <dza724@gmail.com>
2018-08-09 17:03:29 -0700

    Add prints and Image/ filename prefix to AppMain simulator examples. Add AppMains readme.

--- src/AppMains/GaussianBlur/Main.hs
+++ src/AppMains/GaussianBlur/Main.hs
@@ -4,14 +4,20 @@ import Aetherling.Convolution
 import Aetherling.ImageIO
 
 main = do
-  pixels' <- vReadRGB8 "HalideParrot.png"
+  let inFilename = "Images/HalideParrot.png"
+  let outFilename = "Images/GaussianBlur7x7.png"
+
+  putStrLn ("Reading " ++ inFilename)
+  pixels' <- vReadRGB8 inFilename
   let (inImageSize, pixels) =
         case pixels' of
           Left message -> error message
           Right theData -> theData
   let op = MapOp 3 (appsGaussianBlur7 inImageSize) -- Map over RGB.
+  putStrLn "Running simulator on convolution pipeline."
   let ([output], _, warnings) = simulateHighLevel' op [pixels] []
   let outImageSize = (fst inImageSize - 6, snd inImageSize - 6)
 
-  putStr warnings
-  vWriteRGBA8 "AetherlingGaussian.png" inImageSize outImageSize (6,6) output
+  putStrLn warnings
+  putStrLn ("Writing " ++ outFilename)
+  vWriteRGBA8 outFilename inImageSize outImageSize (6,6) output
--- src/AppMains/MipMap/Main.hs
+++ src/AppMains/MipMap/Main.hs
@@ -13,29 +13,33 @@ main = do
   -- RGB MipMap type = int[3]
   let op = appsMipMap (T_Array 3 T_Int) dimLog2 speedLog2
 
-  pixels' <- vParReadRGB8 (2^speedLog2) "HalideParrot256.png"
+  let filename = "Images/HalideParrot256.png"
+  putStrLn ("Reading " ++ filename)
+  pixels' <- vParReadRGB8 (2^speedLog2) filename
   let pixels =
         case pixels' of
           Left message -> error message
           Right (imgSize, pixels) ->
             if imgSize == (256, 256) then pixels
             else error "Expected 256 x 256 image."
+  putStrLn "Running simulation."
   let (_, memOuts, warnings) = simulateHighLevel' op [pixels] []
 
-  putStr warnings
+  putStrLn warnings
   writeMipMaps memOuts 1 (dimLog2-1) (speedLog2-2)
 
 writeMipMaps :: [[ValueType]] -> Int -> Int -> Int -> IO ()
 writeMipMaps memOuts level dimLog2 speedLog2
   | memOuts == [] = error "Ran out of memory outputs."
   | dimLog2 == 0 = do
-      let filename = "MipMapLevels/" ++ show level ++ ".png"
+      let filename = "Images/MipMapLevels/" ++ show level ++ ".png"
+      putStrLn ("Writing " ++ filename)
       vParWriteRGBA8 1 filename (1,1) (1,1) (0,0) (head memOuts)
   | dimLog2 >= 0 = do
       let width = 2^(max 0 speedLog2)
-      let filename = "MipMapLevels/" ++ show level ++ ".png"
+      let filename = "Images/MipMapLevels/" ++ show level ++ ".png"
       let dim = 2^dimLog2
+      putStrLn ("Writing " ++ filename)
       vParWriteRGBA8 width filename (dim, dim) (dim, dim) (0, 0) (head memOuts)
       writeMipMaps (tail memOuts) (level+1) (dimLog2-1) (speedLog2-2)
   | otherwise = error "Negative dimLog2 (fractional image size?)"
-
